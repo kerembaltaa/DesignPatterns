dd17598a9978687f796dc7644239506ade2cb7b6
mustafa kerem balta <mkerembalta@hotmail.com>
2022-09-05 13:06:14 +0300

    Abstract Factory örnekleri eklendi.

---
+++ Abstract Factory/Abstract Factory/Abstract Factory 2/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace Abstract_Factory_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFootballFactory germany = new BundesLigaFactory();
+            IFootballFactory spain = new LaLigaFactory();
+            IFootballFactory italy = new SerieAFactory();
+            FootballWorld footballWorld = new FootballWorld(spain);
+            Console.WriteLine(footballWorld.GetFootballTeamColor());
+            Console.WriteLine(footballWorld.GetTopScorer());
+            Console.ReadLine();
+        }
+    }
+
+    public interface IPlayer
+    {
+        string GetTopScorer();
+    }
+
+    public interface ITeam
+    {
+        string GetTeamColor();
+    }
+
+    public interface IFootballFactory
+    {
+        ITeam CreateTeam();
+        IPlayer CreatePlayer();
+    }
+
+    public class BundesLigaFactory : IFootballFactory
+    {
+        public ITeam CreateTeam()
+        {
+            return new BorussiaDotmund();
+        }
+
+        public IPlayer CreatePlayer()
+        {
+            return new BundesligaPlayer();
+        }
+    }
+
+    public class LaLigaFactory : IFootballFactory
+    {
+        public ITeam CreateTeam()
+        {
+            return new RealMadrid();
+        }
+
+        public IPlayer CreatePlayer()
+        {
+            return new LaLigaPlayer();
+        }
+    }
+
+    public class SerieAFactory : IFootballFactory
+    {
+        public ITeam CreateTeam()
+        {
+            return new Juventus();
+        }
+
+        public IPlayer CreatePlayer()
+        {
+            return new SerieAPlayer();
+        }
+    }
+
+    public class BorussiaDotmund : ITeam
+    {
+        public string GetTeamColor()
+        {
+            return "Black and Yellow";
+        }
+    }
+
+    public class Juventus : ITeam
+    {
+        public string GetTeamColor()
+        {
+            return "Black and White";
+        }
+    }
+
+    public class RealMadrid : ITeam
+    {
+        public string GetTeamColor()
+        {
+            return "Blue and White";
+        }
+    }
+
+    public class BundesligaPlayer : IPlayer
+    {
+        public string GetTopScorer()
+        {
+            return "Robert Lewandowski";
+        }
+    }
+
+    public class LaLigaPlayer : IPlayer
+    {
+        public string GetTopScorer()
+        {
+            return "Lionel Messi";
+        }
+    }
+
+    public class SerieAPlayer : IPlayer
+    {
+        public string GetTopScorer()
+        {
+            return "Cristiano Ronaldo";
+        }
+    }
+
+    public class FootballWorld
+    {
+        private readonly ITeam _team;
+        private readonly IPlayer _teamScorer;
+
+        public FootballWorld(IFootballFactory factory)
+        {
+            _team = factory.CreateTeam();
+            _teamScorer = factory.CreatePlayer();
+        }
+
+        public string GetFootballTeamColor()
+        {
+            return _team.GetTeamColor();
+        }
+
+        public string GetTopScorer()
+        {
+
+            return _teamScorer.GetTopScorer();
+        }
+    }
+}
---
+++ Abstract Factory/Abstract Factory/Abstract Factory/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Abstract_Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Creater create = new Creater(new SqlDatabase());
+            Console.WriteLine("**********");
+            create = new Creater(new MySqlDatabase());
+            Console.Read();
+        }
+    }
+    abstract class Connection
+    {
+        public abstract bool Connect();
+        public abstract bool DisConnect();
+        public abstract string State { get; }
+    }
+
+    abstract class Command
+    {
+        public abstract void Execute(string query);
+    }
+
+    class SqlConnection : Connection
+    {
+        public override string State => "Open";
+        public override bool Connect()
+        {
+            Console.WriteLine("SqlConnection bağlantısı kuruluyor.");
+            return true;
+        }
+        public override bool DisConnect()
+        {
+            Console.WriteLine("SqlConnection bağlantısı kesiliyor.");
+            return false;
+        }
+    }
+
+    class SqlCommand : Command
+    {
+        public override void Execute(string query) => Console.WriteLine("SqlCommand sorgusu çalıştırıldı.");
+    }
+
+    class MySqlConnection : Connection
+    {
+        public override string State => "Open";
+        public override bool Connect()
+        {
+            Console.WriteLine("MySqlConnection bağlantısı kuruluyor.");
+            return true;
+        }
+        public override bool DisConnect()
+        {
+            Console.WriteLine("MySqlConnection bağlantısı kesiliyor.");
+            return false;
+        }
+    }
+
+    class MySqlCommand : Command
+    {
+        public override void Execute(string query) => Console.WriteLine("MySqlCommand sorgusu çalıştırıldı.");
+    }
+
+    abstract class DatabaseFactory
+    {
+        public abstract Connection CreateConnection();
+        public abstract Command CreateCommand();
+    }
+
+    class SqlDatabase : DatabaseFactory
+    {
+        public override Command CreateCommand() => new SqlCommand();
+        public override Connection CreateConnection() => new SqlConnection();
+    }
+
+    class MySqlDatabase : DatabaseFactory
+    {
+        public override Command CreateCommand() => new MySqlCommand();
+        public override Connection CreateConnection() => new MySqlConnection();
+    }
+
+    class Creater
+    {
+        DatabaseFactory _databaseFactory;
+        Connection _connection;
+        Command _command;
+        public Creater(DatabaseFactory databaseFactory)
+        {
+            _databaseFactory = databaseFactory;
+            _connection = _databaseFactory.CreateConnection();
+            _command = _databaseFactory.CreateCommand();
+
+            Start();
+        }
+
+        void Start()
+        {
+            if (_connection.State == "Open")
+            {
+                _connection.Connect();
+                _command.Execute("Select * from...");
+                _connection.DisConnect();
+            }
+        }
+    }
+
+
+}
