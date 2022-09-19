1e904846dd0c321b0d12a6ed20b3c983ac988167
mustafa kerem balta <mkerembalta@hotmail.com>
2022-09-19 13:57:24 +0300

    New Design Pattern

---
+++ Chain Of Responsibility/Chain Of Responsibility DP/Chain Of Responsibility DP2/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace Chain_Of_Responsibility_DP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var withdraw = new Withdraw("a6e193dc-cdbb-4f09-af1a-dea307a9ed15", 480000, "TRY", "TR681223154132432141412");
+
+            Calisan sorumlu = new Sorumlu();
+            Calisan yonetici = new Yonetici();
+            Calisan grupYoneticisi = new GrupYoneticisi();
+            Calisan direktor = new Direktor();
+
+            sorumlu.SiradakiOnayciyiSetEt(yonetici);
+            yonetici.SiradakiOnayciyiSetEt(grupYoneticisi);
+            grupYoneticisi.SiradakiOnayciyiSetEt(direktor);
+
+
+            sorumlu.ProcessRequest(withdraw);
+
+            Console.ReadKey();
+        }
+    }
+    public abstract class Calisan
+    {
+        protected Calisan SıradakiOnayci;
+
+        public void SiradakiOnayciyiSetEt(Calisan employee)
+        {
+            this.SıradakiOnayci = employee;
+        }
+
+        public abstract void ProcessRequest(Withdraw req);
+    }
+    public class Direktor : Calisan
+    {
+        public override void ProcessRequest(Withdraw req)
+        {
+            if (req.Amount <= 750000)
+            {
+                Console.WriteLine("{0} tarafından para çekme işlemi onaylandı #{1} TL", this.GetType().Name, req.Amount);
+            }
+            else
+            {
+                throw new Exception($"Limit banka günlük işlem limitini aştığından para çekme işlemi #{req.Amount} TL onaylanmadı.");
+            }
+        }
+    }
+    public class GrupYoneticisi : Calisan
+    {
+        public override void ProcessRequest(Withdraw req)
+        {
+            if (req.Amount <= 150000)
+            {
+                Console.WriteLine("{0} tarafından para çekme işlemi onaylandı #{1} TL", this.GetType().Name, req.Amount);
+            }
+            else if (SıradakiOnayci != null)
+            {
+                Console.WriteLine("{0} TL işlem tutarı {1} max. limitini aştığından işlem yöneticiye gönderildi.", req.Amount, this.GetType().Name);
+
+                SıradakiOnayci.ProcessRequest(req);
+            }
+        }
+    }
+    public class Yonetici : Calisan
+    {
+        public override void ProcessRequest(Withdraw req)
+        {
+            if (req.Amount <= 70000)
+            {
+                Console.WriteLine("{0} tarafından para çekme işlemi onaylandı #{1} TL", this.GetType().Name, req.Amount);
+            }
+            else if (SıradakiOnayci != null)
+            {
+                Console.WriteLine("{0} TL işlem tutarı {1} max. limitini aştığından işlem yöneticiye gönderildi.", req.Amount, this.GetType().Name);
+
+                SıradakiOnayci.ProcessRequest(req);
+            }
+        }
+    }
+    public class Sorumlu : Calisan
+    {
+        public override void ProcessRequest(Withdraw req)
+        {
+            if (req.Amount <= 40000)
+            {
+                Console.WriteLine("{0} tarafından para çekme işlemi onaylandı #{1}", this.GetType().Name, req.Amount);
+            }
+            else if (SıradakiOnayci != null)
+            {
+                Console.WriteLine("{0} TL işlem tutarı {1} max. limitini aştığından işlem yöneticiye gönderildi.", req.Amount, this.GetType().Name);
+
+                SıradakiOnayci.ProcessRequest(req);
+            }
+        }
+    }
+
+    public class Withdraw
+    {
+        public string CustomerId { get; }
+        public decimal Amount { get; }
+        public string CurrencyType { get; }
+        public string SoruceAccountId { get; }
+
+        public Withdraw(string customerId, decimal amount, string currencyType, string soruceAccountId)
+        {
+            CustomerId = customerId;
+            Amount = amount;
+            CurrencyType = currencyType;
+            SoruceAccountId = soruceAccountId;
+        }
+    }
+
+}
---
+++ Chain Of Responsibility/Chain Of Responsibility DP/Chain Of Responsibility DP3/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chain_Of_Responsibility_DP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IParticipant p3 = new Participant(null) { Name = "Raja" };
+            IParticipant p2 = new Participant(p3) { Name = "Sree" };
+            IParticipant p1 = new Participant(p2) { Name = "Sumesh" };
+            p3.NextParticipent = p1;
+            List<IParticipant> participents = new List<IParticipant>() { p1, p2, p3 };
+
+
+
+            IQuestion q1 = new Question("Is CPU a part of computer?", "Yes");
+            IQuestion q2 = new Question("Is IAS a computer course?", "No");
+            IQuestion q3 = new Question("Is computer an electronic device?", "Yes");
+            List<IQuestion> questions = new List<IQuestion>() { q1, q2, q3 };
+
+
+            QuizHost myQuiz = new QuizHost(questions, participents);
+            myQuiz.StartEvent();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+    class QuizHost
+    {
+        private List<IQuestion> _questions;
+        private List<IParticipant> _participents;
+
+        public QuizHost(List<IQuestion> questions, List<IParticipant> participents)
+        {
+            _questions = questions;
+            _participents = participents;
+        }
+
+        public void StartEvent()
+        {
+            Console.WriteLine("Start of quiz. Welcome All.\n");
+
+            IParticipant currentParticipent = _participents.First();
+
+            foreach (var q in _questions)
+            {
+                q.Owner = currentParticipent;
+                currentParticipent.DoAnswer(q);
+                currentParticipent = currentParticipent.NextParticipent;
+            }
+
+            Console.WriteLine("\nEnd of quiz. Score card.");
+            foreach (var p in _participents)
+            {
+                Console.WriteLine(p.Name + " : " + p.Score);
+            }
+
+            Console.WriteLine("\nThanks for participating.");
+        }
+    }
+
+    interface IQuestion
+    {
+        string Question1 { get; set; }
+        IParticipant Owner { get; set; }
+        bool CheckAnswer(string ans);
+    }
+
+    class Question : IQuestion
+    {
+        public string Question1 { get; set; }
+        public IParticipant Owner { get; set; }
+        private string Answer;
+
+        public Question(string q, string a)
+        {
+            Question1 = q;
+            Answer = a;
+        }
+
+        public bool CheckAnswer(string ans)
+        {
+            return ans.Equals(Answer, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+
+    interface IParticipant
+    {
+        string Name { get; set; }
+        int Score { get; set; }
+        IParticipant NextParticipent { get; set; }
+        void DoAnswer(IQuestion question);
+    }
+
+    class Participant : IParticipant
+    {
+        public IParticipant NextParticipent { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+        public Participant(IParticipant nextParticipent)
+        {
+            NextParticipent = nextParticipent;
+            Score = 0;
+        }
+
+        public void DoAnswer(IQuestion question)
+        {
+            Console.WriteLine("Hi " + Name + ", Please answer for the question " + question.Question1);
+            string ans = Console.ReadLine();
+
+            if (question.CheckAnswer(ans))
+            {
+                Console.WriteLine("Correct Answer");
+                this.Score += 1;
+            }
+            else if (NextParticipent != question.Owner)
+            {
+                Console.WriteLine("Wrong Answer. Pass on to next participent.");
+                NextParticipent.DoAnswer(question);
+            }
+            else
+            {
+                Console.WriteLine("No one answered");
+            }
+        }
+    }
+}
