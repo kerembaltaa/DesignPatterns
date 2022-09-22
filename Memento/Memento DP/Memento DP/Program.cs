f958acbb43e2f784eb2175a588351ab3de3d06d3
mustafa kerem balta <mkerembalta@hotmail.com>
2022-09-22 11:03:14 +0300

    New Design Patterns added.

---
+++ Memento/Memento DP/Memento DP/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Memento_DP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameWorld zulu = new GameWorld { Name = "Zulu", Population = 1000 };
+
+            Console.WriteLine(zulu.ToString());
+
+            GameWorldCareTaker taker = new GameWorldCareTaker();
+            taker.World = zulu.Save(); //zulunun o anki halini sakla
+
+            zulu.Population += 999;
+            Console.WriteLine(zulu.ToString());
+
+            zulu.Undo(taker.World); //eski dünyayi geri yükle
+
+            Console.WriteLine(zulu.ToString());
+
+            Console.ReadKey();
+        }
+        class GameWorld
+        {
+            public string Name { get; set; }
+            public long Population { get; set; }
+
+            //Yeni bir gameworldmomento nesnesi örnekleyip ona orginator classına ait nesnenin ilgili özelliklerini atar.
+            public GameWorldMemento Save()
+            {
+                return new GameWorldMemento { Name = this.Name, Population = this.Population }; //calisma zamanındaki özelllikleri alır.
+            }
+
+            //o anda gelen orginator nesnesinin ilgili özelliklerini set eder
+            public void Undo(GameWorldMemento memento)
+            {
+                this.Name = memento.Name;
+                this.Population = memento.Population;
+            }
+
+            public override string ToString()
+            {
+                return String.Format("{0} dünyasında {1} insan var", Name, Population);
+            }
+        }
+        class GameWorldMemento
+        {
+            public string Name { get; set; }
+            public long Population { get; set; }
+        }
+        class GameWorldCareTaker
+        {
+            public GameWorldMemento World { get; set; } //mementoyu döner.
+        }
+
+    }
+}
---
+++ Memento/Memento DP/Memento DP2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Memento_DP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+// Örnek bir Product nesnesi oluşturulur
+             
+            Product prd = new Product
+            {
+                ProductId = 1000,
+                Name = "Starbucks Kahve Fincanı 330 mililitre",
+                ListPrice = 12
+            };
+            Console.WriteLine(prd.ToString());
+             
+            // Caretaker nesnesi oluşturulur.            
+            Memory memory = new Memory();
+            // Memento nesnesi içeriği o anki Product örneğinden elde edilir.
+            memory.ProductMemento = prd.Save();
+            Console.WriteLine("Product nesnesi kaydedildi.Değişiklik yapılacak.");
+ 
+            prd.ProductId = 9999;
+            prd.Name = "STARBUCKS KAHVE KABI";
+            prd.ListPrice = 24;
+            Console.WriteLine("Yeni hali : \n\t{0}", prd.ToString());
+ 
+            // Restore işlemi gerçekleştirilir
+            prd.Restore(memory.ProductMemento);
+            Console.WriteLine("Undo : \n\t{0}",prd.ToString());        }
+    }
+    // Originator Class
+    // Yaratıcı sınıf
+    class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal ListPrice { get; set; }
+
+        // O anki Product nesne örneğinin içeriğini yeni bir Memento nesne örneğinde toplar ve bunu dış ortama verir.
+        public Memento Save()
+        {
+            return new Memento
+            {
+                ProductId = this.ProductId
+                ,
+                Name = this.Name
+                ,
+                ListPrice = this.ListPrice
+            };
+        }
+
+        // Saklanan Memento nesne örneğini alarak o anki Product nesne örneğinin dahili içeriğinin doldurulmasında kullanılır.
+        public void Restore(Memento memento)
+        {
+            this.ListPrice = memento.ListPrice;
+            this.Name = memento.Name;
+            this.ProductId = memento.ProductId;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} : {1} ( {2} )", ProductId, Name, ListPrice.ToString("C2"));
+        }
+    }
+
+
+    // Memento Class
+    // Akıl defteri sınıfı
+    // Product tipi içerisinden saklanmak amacıyla kullanılacak tüm özellikleri tanımlar
+    class Memento
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal ListPrice { get; set; }
+    }
+
+    // Caretaker class
+    // Bak��cı sınıf
+    class Memory
+    {
+        public Memento ProductMemento { get; set; }
+    }
+}
