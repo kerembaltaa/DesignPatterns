4a4817f311e8cd3c9709931db000b85161adbea2
mustafa kerem balta <mkerembalta@hotmail.com>
2022-09-23 12:04:58 +0300

    Template Method DP added.

---
+++ Template Method/Template Method DP/Template Method DP/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Template_Method_DP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alisveris a1 = new Televizyon();
+            a1.TemplateMethod();
+            Console.WriteLine("***********");
+            Alisveris a2 = new Buzdolabi();
+            a2.TemplateMethod();
+
+            Console.Read();
+        }
+    }
+    enum OdemeTipi
+    {
+        Pesin, Taksit
+    }
+
+    abstract class Alisveris
+    {
+        protected string UrunAdi;
+        protected OdemeTipi odemeTipi;
+        void Baslat() => Console.WriteLine("Alışveriş başladı.");
+        void Bitir() => Console.WriteLine($"Alışveriş bitti.{UrunAdi} {odemeTipi} yöntemiyle alınmıştır.");
+        abstract public void Urun();
+        abstract public void OdemeSekli();
+        public void TemplateMethod()
+        {
+            Baslat();
+            Urun();
+            OdemeSekli();
+            Bitir();
+        }
+    }
+    class Televizyon : Alisveris
+    {
+        public override void OdemeSekli()
+        {
+            odemeTipi = OdemeTipi.Pesin;
+        }
+
+        public override void Urun()
+        {
+            UrunAdi = "Televizyon";
+        }
+    }
+
+    class Buzdolabi : Alisveris
+    {
+        public override void OdemeSekli()
+        {
+            odemeTipi = OdemeTipi.Taksit;
+        }
+
+        public override void Urun()
+        {
+            UrunAdi = "Buzdolabı";
+        }
+    }
+
+}
---
+++ Template Method/Template Method DP/Template Method DP2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Template_Method_DP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameReporter reporter = null;
+
+            reporter = new XmlReporter();
+            reporter.WriteSummary();
+            Console.WriteLine();
+
+            reporter = new TextReporter();
+            reporter.WriteSummary();
+            Console.WriteLine();
+
+            reporter = new ConsoleReporter();
+            reporter.WriteSummary();
+        }
+    }
+    abstract class GameReporter
+    {
+        public void GetResults()
+        {
+            Console.WriteLine("Oyuncuların istatistikleri toplanıyor");
+        }
+
+        public void ParseResults()
+        {
+            Console.WriteLine("İstatistikler ayrıştırılıyor");
+        }
+
+        public abstract void WriteResults();
+
+        public void WriteSummary()
+        {
+            GetResults();
+            ParseResults();
+            WriteResults();
+        }
+    }
+
+    class XmlReporter
+        : GameReporter
+    {
+        public override void WriteResults()
+        {
+            Console.WriteLine("İstatistikler XML dosyasına yazılıyor.");
+        }
+    }
+
+    class TextReporter
+        : GameReporter
+    {
+        public override void WriteResults()
+        {
+            Console.WriteLine("İstatistikler TEXT dosyasına yazdırılıyor.");
+        }
+    }
+
+    class ConsoleReporter
+        : GameReporter
+    {
+        public override void WriteResults()
+        {
+            Console.WriteLine("İstatistikler CONSOLE ekranına basılıyor.");
+        }
+    }
+}
