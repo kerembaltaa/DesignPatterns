cad08b764a0a5b221f90a69f2eb5c1397e2629e6
mustafa kerem balta <mkerembalta@hotmail.com>
2022-09-23 10:29:40 +0300

    Strategy DP added.

---
+++ Strategy/Strategy DP/Strategy DP/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Strategy_DP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serializer srz = new Serializer(new XmlSerializer());
+
+            srz.Serialize("Strategy");
+            srz.Deserialize("Strategy");
+
+            Console.WriteLine();
+
+            srz = new Serializer(new BinarySerializer());
+
+            srz.Serialize("");
+            srz.Deserialize("");
+
+            Console.WriteLine();
+
+            srz = new Serializer(new JsonSerializer());
+
+            srz.Serialize("New object");
+            srz.Deserialize("New object");
+
+        }
+    }
+    //Base Interface
+    interface ISerializable
+    {
+        void Serialize(string str);
+        void Deserialize(string str);
+    }
+
+    //Concrete tip 1
+    class XmlSerializer : ISerializable
+    {
+        public void Deserialize(string str)
+        {
+            Console.WriteLine("xml ters serileştirme");
+        }
+
+        public void Serialize(string str)
+        {
+            Console.WriteLine("xml serileştirme");
+        }
+    }
+
+    //Concrete tip 2
+    class BinarySerializer : ISerializable
+    {
+        public void Deserialize(string str)
+        {
+            Console.WriteLine("binary ters serileştirme");
+        }
+
+        public void Serialize(string str)
+        {
+            Console.WriteLine("binary serileştirme");
+        }
+    }
+
+    //Concrete tip 3(yeni eklenen)
+    class JsonSerializer : ISerializable
+    {
+        public void Deserialize(string str)
+        {
+            Console.WriteLine("json ters serileştirme");
+        }
+
+        public void Serialize(string str)
+        {
+            Console.WriteLine("json serileştirme");
+        }
+    }
+
+    //Context tip
+    class Serializer
+    {
+        ISerializable _serializer;
+
+        public Serializer(ISerializable serializer)
+        {
+            _serializer = serializer;
+        }
+
+        public void Serialize(string str)
+        {
+            _serializer.Serialize(str);
+        }
+
+        public void Deserialize(string str)
+        {
+            _serializer.Deserialize(str);
+        }
+    }
+}
---
+++ Strategy/Strategy DP/Strategy DP2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Strategy_DP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "<app><config><sqlConnection>data....</sqlConnection></config></app>";
+
+            Encrypter enc1 = new Encrypter(new TripleDesEncrypter());
+            string encryptedStr = enc1.Encrypt(str);
+            string decryptedStr = enc1.Decyrpt(str);
+
+            enc1 = new Encrypter(new RijndaelEncrypter());
+            encryptedStr = enc1.Encrypt(str);
+            decryptedStr = enc1.Decyrpt(str);
+        }
+    }
+    // Strategy type
+    interface IEncrypter
+    {
+        string Encrypt(string obj);
+        string Decyrpt(string obj);
+    }
+
+    // ConcreteStrategy type 1
+    class RijndaelEncrypter
+        : IEncrypter
+    {
+        #region IEncrypter Members
+
+        public string Encrypt(string obj)
+        {
+            Console.WriteLine("obj için Rijndael şifreleme");
+            return obj;
+        }
+
+        public string Decyrpt(string obj)
+        {
+            Console.WriteLine("obj için Rijndael ters şifreleme");
+            return obj;
+        }
+
+        #endregion
+    }
+
+    // ConcreteStrategy type 1
+    class TripleDesEncrypter
+        : IEncrypter
+    {
+        #region IEncrypter Members
+
+        public string Encrypt(string obj)
+        {
+            Console.WriteLine("obj için TripleDES şifreleme");
+            return obj;
+        }
+
+        public string Decyrpt(string obj)
+        {
+            Console.WriteLine("obj için TripleDES ters şifreleme");
+            return obj;
+        }
+
+        #endregion
+    }
+
+    // Context Type
+    class Encrypter
+    {
+        IEncrypter _enc = null;
+
+        public Encrypter(IEncrypter enc)
+        {
+            _enc = enc;
+        }
+
+        public string Encrypt(string obj)
+        {
+            return _enc.Encrypt(obj);
+        }
+        public string Decyrpt(string obj)
+        {
+            return _enc.Decyrpt(obj);
+        }
+    }
+}
