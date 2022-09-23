7d18c1a6ceaa4f84f6af4c6b49d0bf3deaaa1dfb
mustafa kerem balta <mkerembalta@hotmail.com>
2022-09-23 13:03:25 +0300

    New DPs added

---
+++ Visitor/Visitor DP/Visitor DP/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Visitor_DP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IStore> store = new List<IStore>();
+            IStore car1 = new Car() { CarName = "A1", Price = 200M, CarType = "Mercedes" };
+            IStore car2 = new Car() { CarName = "A2", Price = 100M, CarType = "Normal" };
+
+            IStore bike1 = new Bike() { BikeName = "B1", Price = 50M, BikeType = "Bullet" };
+            IStore bike2 = new Bike() { BikeName = "B2", Price = 30M, BikeType = "Normal" };
+
+
+            PriceVisitor priceVisitor = new PriceVisitor();
+            car1.Visit(priceVisitor);
+            car2.Visit(priceVisitor);
+
+            bike1.Visit(priceVisitor);
+            bike2.Visit(priceVisitor);
+
+
+            WeightVisitor weightVisitor = new WeightVisitor();
+            car1.Visit(weightVisitor);
+            car2.Visit(weightVisitor);
+
+            bike1.Visit(weightVisitor);
+            bike2.Visit(weightVisitor);
+
+            Console.ReadLine();
+        }
+    }
+    public interface IStore
+    {
+        void Visit(IVisitor visitor);
+    }
+    public class Car : IStore
+    {
+        public string CarName { get; set; }
+        public decimal Price { get; set; }
+        public string CarType { get; set; }
+
+        public void Visit(IVisitor visitor)
+        {
+            visitor.Accept(this);
+        }
+    }
+    public class Bike : IStore
+    {
+        public string BikeName { get; set; }
+        public decimal Price { get; set; }
+        public string BikeType { get; set; }
+
+        public void Visit(IVisitor visitor)
+        {
+            visitor.Accept(this);
+        }
+    }
+    public interface IVisitor
+    {
+        void Accept(Car car);
+        void Accept(Bike bike);
+    }
+    public class PriceVisitor : IVisitor
+    {
+        private const int CAR_DISCOUNT = 5;
+        private const int BIKE_DISCOUNT = 2;
+
+        public void Accept(Car car)
+        {
+            decimal carPriceAfterDicount = car.Price - ((car.Price / 100) * CAR_DISCOUNT);
+            Console.WriteLine("Car {0} price: {1}", car.CarName, carPriceAfterDicount);
+        }
+
+        public void Accept(Bike bike)
+        {
+            decimal bikePriceAfterDicount = bike.Price - ((bike.Price / 100) * BIKE_DISCOUNT);
+            Console.WriteLine("Bike {0} price: {1}", bike.BikeName, bikePriceAfterDicount);
+        }
+    }
+    public class WeightVisitor : IVisitor
+    {
+        public void Accept(Car car)
+        {
+            switch (car.CarType)
+            {
+                case "Mercedes":
+                    Console.WriteLine("Car {0} weight: {1} KG", car.CarName, 1750);
+                    break;
+                case "Normal":
+                    Console.WriteLine("Car {0} weight: {1} KG", car.CarName, 750);
+                    break;
+            }
+        }
+
+        public void Accept(Bike bike)
+        {
+            switch (bike.BikeType)
+            {
+                case "Bullet":
+                    Console.WriteLine("Bike {0} weight: {1} KG", bike.BikeName, 300);
+                    break;
+                case "Normal":
+                    Console.WriteLine("Bike {0} weight: {1} KG", bike.BikeName, 100);
+                    break;
+            }
+        }
+    }
+}
---
+++ Visitor/Visitor DP/Visitor DP2/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Visitor_DP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPad iPad = new IPad("Ipad mini", "Apple");
+            GalaxyTab galaxyTab = new GalaxyTab("Galaxy Tab", "Samsung");
+
+            iPad.Accept(new WifiVisitor());
+            galaxyTab.Accept(new WifiVisitor());
+
+            iPad.Accept(new ThreeGVisitor());
+            galaxyTab.Accept(new ThreeGVisitor());
+
+            //bunun gibi baska visitor sınıfları yazarak sınıfımızı değiştirmeden
+            //yeni metotlar çalıştırabilir hale getirebiliriz. 
+
+            Console.ReadLine();
+        }
+    }
+    /// <summary>
+    /// The 'Tablet' abstract class
+    /// </summary>
+    public abstract class Tablet
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+
+        public Tablet(string model, string brand)
+        {
+            Model = model;
+            Brand = brand;
+        }
+
+        public abstract void Accept(IVisitor visitor);
+    }
+
+    /// <summary>
+    /// The 'ConcreteElement' class
+    /// </summary>
+    public class IPad : Tablet
+    {
+        public IPad(string model, string brand)
+            : base(model, brand)
+        {
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteElement' class
+    /// </summary>
+    public class GalaxyTab : Tablet
+    {
+        public GalaxyTab(string model, string brand)
+            : base(model, brand)
+        {
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    /// <summary>
+    /// The 'Visitor' interface
+    /// </summary>
+    public interface IVisitor
+    {
+        void Visit(Tablet tablet);
+    }
+
+    /// <summary>
+    /// A 'ConcreteVisitor' class
+    /// </summary>
+    public class WifiVisitor : IVisitor
+    {
+        public void Visit(Tablet tablet)
+        {
+            if (tablet is IPad)
+                Console.WriteLine("Ipad wifi has open.");
+            else if (tablet is GalaxyTab)
+                Console.WriteLine("GalaxyTab does not have wifi option.");
+            else
+                Console.WriteLine("This object is not a tablet.");
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteVisitor' class
+    /// </summary>
+    public class ThreeGVisitor : IVisitor
+    {
+        public void Visit(Tablet tablet)
+        {
+            if (tablet is IPad)
+                Console.WriteLine("Ipad wifi does not have 3G option.");
+            else if (tablet is GalaxyTab)
+                Console.WriteLine("GalaxyTab 3G has open.");
+            else
+                Console.WriteLine("This object is not a tablet.");
+        }
+    }
+}
